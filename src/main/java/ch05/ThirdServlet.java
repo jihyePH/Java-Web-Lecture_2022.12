55ec7ac5c23a89360ef638497e9de03cf97817de
jihyePH <ppjihye0405@gmail.com>
2022-12-07 17:24:30 +0900

    first commit

---
+++ src/main/java/ch05/ThirdServlet.java
@@ -0,0 +1,43 @@
+package ch05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ */
+@WebServlet("/ch05/third")
+public class ThirdServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<!DOCTYPE html>\r\n"
+				+ "<html lang=\"en\">"
+				+ "<head>\r\n"
+				+ "    <meta charset=\"UTF-8\">"
+				+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n"
+				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+				+ "    <title>Document</title>\r\n"
+				+ "</head>\r\n"
+				+ "<body>\r\n"
+				+ "    <h1>안녕하세요!!!</h1>\r\n"
+				+ "</body>\r\n"
+				+ "</html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
